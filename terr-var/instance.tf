47d294f7f7f6c400270214f108ad46f7c823558d
NIKHIL CHOPRA <69970952+TESLAOPUnix@users.noreply.github.com>
2023-10-18 14:51:39 +0530

    Update instance.tf

--- terr-var/instance.tf
+++ terr-var/instance.tf
@@ -1,12 +1,36 @@
-resource "aws_instance" "dove-inst" {
-  ami                    = var.AMIS[var.REGION]
-  instance_type          = "t2.micro"
-  availability_zone      = var.ZONE1
-  key_name               = "dove"
-  vpc_security_group_ids = ["sg-0eec2471b2f16c919"]
-  tags = {
-    Name    = "dove"
-    Project = "terr"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "masub" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = "true"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
   }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.masub.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.masub.id
+  key_name      = "k8s"
 }
 
