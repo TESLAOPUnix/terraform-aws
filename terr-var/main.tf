23da2e02dc6ae24d47a26122fe2c2a06be2b5593
NIKHIL CHOPRA <69970952+TESLAOPUnix@users.noreply.github.com>
2023-11-02 17:03:27 +0530

    added nat gateway

--- terr-var/main.tf
+++ terr-var/main.tf
@@ -9,6 +9,12 @@ resource "aws_subnet" "masub" {
   map_public_ip_on_launch = "true"
 }
 
+resource "aws_subnet" "pvtsub" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = "false"
+}
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
@@ -27,10 +33,112 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.example.id
 }
 
+
+resource "aws_route_table" "pvtsubrtb" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
+  }
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.pvtsub.id
+  route_table_id = aws_route_table.pvtsubrtb.id
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.pubnatip.id
+  subnet_id     = aws_subnet.masub.id
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_eip" "pubnatip" {
+   domain = "vpc"
+
+   depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_security_group" "allow_tls" {
+    name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }      
+}
+
+resource "aws_security_group" "allow_tls_db" {
+  name        = "allow_tls_db"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }      
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami-name
   instance_type = var.inst-type
   subnet_id     = aws_subnet.masub.id
   key_name      = var.key-name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  associate_public_ip_address = true
 }
+
+resource "aws_instance" "db" {
+  ami = var.ami-name
+  instance_type = var.inst-type
+  subnet_id = aws_subnet.pvtsub.id
+  key_name = var.key-name
+  vpc_security_group_ids = [aws_security_group.allow_tls_db.id]
+}
+
+
+
+
+
+
 
